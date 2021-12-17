f7d3cecece911daf0a5b3025ca1b284b04d06b90
jvincentbasto <jvincentbasto@gmail.com>
2021-12-17 10:02:35 +0800

    test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,8 +10,7 @@
   <div class="w-11/12 min-w-min mx-auto h-full flex justify-center items-center">
     <div class="w-full mx-auto mt-36">
       <h1 class="w-full text-center text-7xl font-bold mb-8">Laravel CRUD Operations</h1>
-      {{-- <h3 class="w-full text-center text-4xl font-bold text-zinc-500">Made it very simple!</h3> --}}
-      
+      <h3 class="w-full text-center text-4xl font-bold text-zinc-500">simple and dynamic!</h3>
     </div>
   </div>
 @endsection
