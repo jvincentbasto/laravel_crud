dab6293aadc615a6a5ba88bc9944156f82ca5b6a
jvincentbasto <jvincentbasto@gmail.com>
2021-12-17 13:54:50 +0800

    seeders

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(1),
+            'author' => $this->faker->name(),
+            'published' => $this->faker->date('Y_m_d'),
+            'synopsis' => $this->faker->paragraphs(3,true),
+            'genres' => $this->faker->words(3,true),
+            'price' => $this->faker->randomNumber(7),
+        ];
+    }
+}
